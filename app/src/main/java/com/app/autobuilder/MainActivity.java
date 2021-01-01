07f57469f7c896913ea4bbfbe361e1f50d2c5e92
pi667i <66825857@qq.com>
2021-01-01 19:56:38 +0800

    AutoBuilder 项目初始化

---
+++ app/src/main/java/com/app/autobuilder/MainActivity.java
@@ -0,0 +1,15 @@
+package com.app.autobuilder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        https://blog.csdn.net/hanhailong726188/article/details/46738929
+    }
+}
